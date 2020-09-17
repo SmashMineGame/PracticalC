cee8fb150ae1e18d115024f567944b325b41cb84
smashMineGame <aryanp@khanlabschool.org>
2020-09-17 13:02:04 -0700

    started collatz project

---
+++ Collatz/stoppingTime.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int collatz(int n) {
+	int count = 0;
+
+	while (n != 1) {
+		if (n % 2 == 0) {n /= 2;}
+		else {n = 3*n + 1;}
+		count++;
+	}
+
+	return count;
+}
+
+int findMaxCollatz(int max) {
+	int running = 0;
+	int i;
+	for (i = max / 2; i <= max; i++) {
+		int cur = collatz(i);
+		if (cur > running) {running = i;}
+	}
+	return running;
+}
+
+int main(){
+	int maximumVal = 100000;
+	
+	int maximumFound = findMaxCollatz(maximumVal);	
+	printf("%d",maximumFound);
+	return 0;
+}
